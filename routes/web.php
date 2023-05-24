5f85023fde877fc0add000cee5b9a0e4623a78f4
Ruslan <s19120135@alumnos.itsur.edu.mx>
2023-05-24 11:29:38 -0600

    Tabla de datos

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AlumnoController::class, 'index']);
 
 Route::resource('/alumnos', AlumnoController::class);
